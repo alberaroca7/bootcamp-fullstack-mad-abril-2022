71a7e81ef362f72c05de0e20491d526dfe2fda79
alberaroca7 <alberaroca97@gmail.com>
2022-04-24 12:45:32 +0200

    no message

--- students/alberto-aroca-garcia/ej-js-21-04/ej-tarde/script.js
+++ students/alberto-aroca-garcia/ej-js-21-04/ej-tarde/script.js
@@ -196,6 +196,94 @@
 
 // 7. Crear una página web que solicite al usuario 1 número (n) y cree en HTML  un tablero de ajedrez de nxn, teniendo  en cuenta los colores como en el ajedrez.
 
+// EJERCICIOS DIA 22/04
 
+// 1. Mediante el uso del bucle  while imprime 40 p's con los textos numerados de la siguiente manera:
+// 1 hello world loop
+// 2 hello world loop
+// 3 hello world loop
+// ...
+// 37 hello world loop
 
+/**
+ * Input: number que le pasamos por una varible n que indica el numero de p´s que tiene que imprimir por pantalla
+ * salida: una lista de p´s que dentro contienen un texto numerado (hello world loop) 
+ * 
+ * Ejemplo: n = 40
+ * //
+ *  1 hello world loop
+ *  2 hello world loop
+ *  3 hello world loop
+ *   ...
+ *  37 hello world loop
+ */
 
+// let n = 40;
+// let cont = 1; 
+//     while(cont<=n){
+//         document.write(`<p>${cont}. hello world loop</p>`);
+//         cont++;
+//     }
+
+// 2. Mediante el uso de while crea 100 divs de color #F47732. Modificar el ejercicio para que los divs pares se sean de  color #F47732 
+// y los impares #1148F3. Modificar para que  se muestren sobre un contenedor flex.
+
+/**
+ *  primer parte del ejercicio:
+*       Input: number que le pasamos por una varible n que indica el numero de div´s que tiene que imprimir por pantalla
+*       salida: una lista de div´s de color #F47732.
+ * 
+ *  procedimiento:
+ *      variable n= 100; 
+ *      variable contador = 1;
+ *      bucle while que vaya hasta n, en este caso 100
+ *      con document write pintar los divs (dentro de los divs una clase css que tenga el color y displaly flex)
+ *      incrementar la variable contador 1.
+ */
+
+// const n= 100;
+// let cont=1;
+
+// while (cont<=n) {
+//     document.write(`<div class="divproperties"></div>`);
+//     cont++;
+// }
+
+/**
+ *  segunda parte del ejercicio
+ *      input: number que le pasamos por una variable n que indica el numero de div´s que tiene que imprimir por pantalla
+ *      salida: una serie de divs que en este caso sabemos que tienen que ser 100. 
+ *      en el que los pares sean de color #F47732 y los impares de otro color. #1148F3
+ *      
+ *      - tendremos un contenedor div con display flex
+ *      - dentro el bucle while con la condicion de que mientras cont sea menor o igual que 100 pinte los divs
+ *      - dentro un if else:
+ *               1. que si cont es numero par sea de un color
+ *               2. que si cont es numero impar sea de otro color distinto
+ */
+
+// const n= 100;
+// let cont=1;
+
+// document.write('<div class="div_container">');
+//     while (cont<=n) {
+//         if (cont%2===0) {
+//             document.write(`<div class="div_properties_odd"></div>`);
+//             cont++;
+//         } else{
+//             document.write(`<div class="div_properties_even"></div>`);
+//             cont++;
+//         }
+        
+//     }
+
+// document.write('</div">');
+
+//3. Solicitar el número al usuario un numero por prompt, e imprime en un P  el número al revés.
+
+/**
+ * 
+ * 
+ */
+
+// const n = prompt('');
